9feec0a3db4bdb5c6eea3a222059d65bf84b6abb
Manuel Giffels <giffels@gmail.com>
2019-08-07 14:04:38 +0200

    Implement compute tests and fix datetime vs string in yaml

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -5,4 +5,28 @@ from unittest import TestCase
 
 class TestAzureComputeResource(TestCase):
     def setUp(self) -> None:
-        self.compute = AzureComputeResource()
+        self.compute = AzureComputeResource(api_root_url='https://test.azure.com')
+
+    def test_actions(self) -> None:
+        expected_actions = {'create_or_update': {'method': 'PUT',
+                                                 'url': 'Microsoft.Compute/virtualMachines/{}'},
+                            'delete': {'method': 'DELETE', 'url': 'Microsoft.Compute/virtualMachines/{}'},
+                            'instance_view': {'method': 'GET',
+                                              'url': 'Microsoft.Compute/virtualMachines/{}/instanceView'},
+                            'power_off': {'method': 'POST',
+                                          'url': 'Microsoft.Compute/virtualMachines/{}/powerOff'}}
+        self.assertEqual(self.compute.actions, expected_actions)
+
+    def test_get_action(self) -> None:
+        self.assertEqual(self.compute.get_action('power_off'),
+                         {'method': 'POST', 'url': 'Microsoft.Compute/virtualMachines/{}/powerOff'})
+
+    def test_get_action_full_url(self) -> None:
+        self.assertEqual(self.compute.get_action_full_url('power_off', 'test_vm_name'),
+                         'https://test.azure.com/Microsoft.Compute/virtualMachines/test_vm_name/powerOff')
+
+    def test_get_action_method(self) -> None:
+        self.assertEqual(self.compute.get_action_method('power_off'), 'POST')
+
+    def test_api_version_param(self) -> None:
+        self.assertEqual(self.compute.params, {"api-version": "2018-06-01"})
